c36dc084f64568629255357787789c1ffa593d32
tabish.khan <tabish.khan@tothenew.com>
2017-08-02 15:24:18 +0530

    Added mongolib URL

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,4 +1,6 @@
 var http = require('http');
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/newsfeed";
+//var url = "mongodb://localhost:27017/newsfeed";  // local
+var url = "mongodb://tabblack:blackblack@ds127993.mlab.com:27993/newsfeed";  // Prod
+
 module.exports = url;
